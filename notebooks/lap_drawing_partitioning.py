ea6a05dbc45115f59b0b879a528df8c578c10d0e
bdpedigo <benjamindpedigo@gmail.com>
2021-02-12 14:32:47 -0500

    update lap notebook

--- notebooks/lap_drawing_partitioning.py
+++ notebooks/lap_drawing_partitioning.py
@@ -20,44 +20,12 @@ def eig(A):
 
 
 #%%
-g = nx.davis_southern_women_graph()
-
-fig, axs = plt.subplots(1, 2, figsize=(8, 4))
-ax = axs[0]
-nx.draw_random(g, ax=ax)
-ax.set(title="Random")
-
-ax = axs[1]
-nx.draw_spectral(g, ax=ax)
-ax.set(title="Spectral")
-
-
-nodelist = list(sorted(g.nodes()))
-A = nx.to_numpy_array(g, nodelist=nodelist)
-degrees = np.sum(A, axis=0)
-D = np.diag(degrees)
-D_inv = np.diag(1 / degrees)
-L = D - A
-L_gen = D_inv @ L
-evals, evecs = np.linalg.eig(L_gen)
-sort_inds = np.argsort(evals)
-evals = evals[sort_inds]
-evecs = evecs[:, sort_inds]
-
-locations = evecs[:, 1:3]
-
-pos = dict(zip(nodelist, locations))
-plt.figure()
-nx.draw(g, pos=pos)
-
-#%%
-
-
 B = np.array([[0.8, 0.05], [0.05, 0.8]])
 A = sbm([10, 10], B)
 heatmap(A)
 
-
+#%%
+sns.set_context("talk")
 degrees = np.sum(A, axis=0)
 D = np.diag(degrees)
 L = D - A
@@ -66,9 +34,8 @@ fig = plt.figure()
 sns.scatterplot(y=evals, x=np.arange(len(evals)))
 
 #%%
-
 rows = []
-for p in np.linspace(0, 0.5, 20):
+for p in np.linspace(0, 0.8, 20):
     for i in range(10):
         B = np.array([[0.8, p], [p, 0.8]])
         A = sbm([10, 10], B)
@@ -97,19 +64,59 @@ A = adj.values
 degrees = np.sum(A, axis=0)
 D = np.diag(degrees)
 L = D - A
-
 evals, evecs = eig(L)
 
 #%%
 fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-hue = meta[6]
-sns.scatterplot(y=evecs[:, 6], x=np.arange(len(evecs[:, 6])), hue=hue, ax=ax)
+sns.scatterplot(y=evecs[:, 1], x=np.arange(len(evecs[:, 1])), ax=ax)
+ax.set(xlabel="Index (node)", ylabel="Value in eigenvector")
 
 #%%
-is_pharynx = meta[6]
-
-heatmap(A, inner_hier_labels=is_pharynx)
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+hue = meta[6].values
+sns.scatterplot(y=evecs[:, 1], x=np.arange(len(evecs[:, 1])), hue=hue, ax=ax)
 
 #%%
-g = nx.from_numpy_array(A)
-nx.draw_spring(g)
+# janky code to look at the adjacency matrix sorted this way.
+# not sure why there is a nan
+is_pharynx = list(meta[6].values)
+for i, val in enumerate(is_pharynx):
+    if val not in ["pharynx", "nonpharynx", "linker"]:
+        is_pharynx[i] = "nonpharynx"
+heatmap(
+    A, inner_hier_labels=is_pharynx, transform="simple-nonzero", hier_label_fontsize=12
+)
+
+#%%
+g = nx.davis_southern_women_graph()
+
+fig, axs = plt.subplots(1, 2, figsize=(8, 4))
+ax = axs[0]
+nx.draw_random(g, ax=ax)
+ax.set(title="Random")
+
+ax = axs[1]
+nx.draw_spectral(g, ax=ax)
+ax.set(title="Spectral")
+
+
+# showing that we can indeed get back the same spectral graph layout as networkx
+# (up to a rotation)
+nodelist = list(sorted(g.nodes()))
+A = nx.to_numpy_array(g, nodelist=nodelist)
+degrees = np.sum(A, axis=0)
+D = np.diag(degrees)
+D_inv = np.diag(1 / degrees)
+L = D - A
+L_gen = D_inv @ L
+evals, evecs = np.linalg.eig(L_gen)
+sort_inds = np.argsort(evals)
+evals = evals[sort_inds]
+evecs = evecs[:, sort_inds]
+
+locations = evecs[:, 1:3]
+
+pos = dict(zip(nodelist, locations))
+fig, ax = plt.subplots(1, 1, figsize=(4, 4))
+nx.draw(g, pos=pos, ax=ax)
+ax.set(title="My spectral")
